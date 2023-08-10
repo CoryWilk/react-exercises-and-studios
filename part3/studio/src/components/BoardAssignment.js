01818c941091efa16717800359474f15720b66f9
Cory <standardvr@gmail.com>
2023-08-10 18:50:17 -0500

    Finished Studio

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [
+      {label: "Dessert", value: "Dessert"},
+      {label: "Quick", value: "Quick"},
+      {label: "Easy", value: "Easy"}
+   ];
+
+   const [boardName, setName] = useState("No Boards Yet");
 
    const handleChange = (event) => {
+      setName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boards.map((board) => (
+            <option value={board.value}>{board.label}</option>
+         ))}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,11 +1,11 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.allrecipes.com/cook/12381381";
+   let authorPhoto = "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcam-image-store.accountservices.meredithcorp.io%2Feaacd7e12824861b18e368003ab89e5a8f82f94a&w=320&h=320&c=sc&poi=face&q=60&orient=true";
+   let authorName = "Mary-Helen Matthews";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "Mary-Helen Matthews" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
             <a href={authorLink}></a> 
@@ -15,7 +15,7 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["Condensed Milk", "Cool Whip", "Cream Cheese", "Lemon Juice", "Graham Cracker Crust"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -34,8 +34,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>5-Ingredient No-Bake Cheesecake</h1>
+            <p>Easy 5-ingredient no-bake cheesecake. It's all about chemistry. </p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +47,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://www.allrecipes.com/thmb/ZtpQeqXeqihUREV7JgkKcRP_mGs=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/2760745-ingredient-no-bake-cheesecakelela-8a773a5db2814ccfb0116894d43a5a1b.jpeg" alt="No-Bake Cheesecake" className="imageUpdates"/>
    );
 }
 
